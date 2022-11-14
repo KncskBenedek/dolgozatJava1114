5ba3551b8e3207dc6fbd345c27c14b591d4fc4bc
KncskBenedek <koncsikbenedek@gmail.com>
2022-11-14 11:28:53 +0100

    ujra, kilep event kódból ,űködik

--- src/view/gui.java
+++ src/view/gui.java
@@ -1,9 +1,12 @@
-
 package view;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.List;
+import java.util.ArrayList;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
@@ -14,6 +17,11 @@ public class gui extends javax.swing.JFrame {
 
     private JButton[] gombokPin;
     private Color alapSzin;
+    private ArrayList<JButton> gombokAmoba;
+    private char jel;
+    private JFrame frame = this;
+    private final char KOR = 'O';
+    private final char KERESZT = 'X';
 
     public gui() {
         initComponents();
@@ -30,6 +38,7 @@ public class gui extends javax.swing.JFrame {
     private void initComponents() {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
+        buttonGroup2 = new javax.swing.ButtonGroup();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel4 = new javax.swing.JPanel();
         pinPanel = new javax.swing.JPanel();
@@ -43,7 +52,7 @@ public class gui extends javax.swing.JFrame {
         jList2 = new javax.swing.JList<>();
         jRadioButton3 = new javax.swing.JRadioButton();
         jRadioButton4 = new javax.swing.JRadioButton();
-        jPanel6 = new javax.swing.JPanel();
+        amobaPanel = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jmProgram = new javax.swing.JMenu();
         jmiUjra = new javax.swing.JMenuItem();
@@ -146,10 +155,20 @@ public class gui extends javax.swing.JFrame {
 
         buttonGroup1.add(jRadioButton3);
         jRadioButton3.setSelected(true);
-        jRadioButton3.setText("xkezd");
+        jRadioButton3.setText("X kezd");
+        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton3ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton4);
-        jRadioButton4.setText("y kezd");
+        jRadioButton4.setText("O kezd");
+        jRadioButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -174,17 +193,17 @@ public class gui extends javax.swing.JFrame {
                 .addContainerGap(23, Short.MAX_VALUE))
         );
 
-        jPanel6.setBorder(javax.swing.BorderFactory.createTitledBorder("amőba"));
+        amobaPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("amőba"));
 
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+        javax.swing.GroupLayout amobaPanelLayout = new javax.swing.GroupLayout(amobaPanel);
+        amobaPanel.setLayout(amobaPanelLayout);
+        amobaPanelLayout.setHorizontalGroup(
+            amobaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 117, Short.MAX_VALUE)
         );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+        amobaPanelLayout.setVerticalGroup(
+            amobaPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 108, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
@@ -192,9 +211,9 @@ public class gui extends javax.swing.JFrame {
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
-                .addGap(53, 53, 53)
-                .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(amobaPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(56, 56, 56))
         );
@@ -207,7 +226,7 @@ public class gui extends javax.swing.JFrame {
                         .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel5Layout.createSequentialGroup()
                         .addGap(77, 77, 77)
-                        .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(amobaPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(76, Short.MAX_VALUE))
         );
 
@@ -216,30 +235,21 @@ public class gui extends javax.swing.JFrame {
         jmProgram.setText("Program");
 
         jmiUjra.setText("Újra");
-        jmiUjra.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jmiUjraActionPerformed(evt);
-            }
-        });
         jmProgram.add(jmiUjra);
 
         jmiKilep.setText("Kilép");
-        jmiKilep.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jmiKilepActionPerformed(evt);
-            }
-        });
         jmProgram.add(jmiKilep);
 
         jMenuBar1.add(jmProgram);
 
         jMenu2.setText("Játék elrendezés");
 
+        buttonGroup2.add(jrbVizszintes);
         jrbVizszintes.setSelected(true);
         jrbVizszintes.setText("vízszintes");
         jMenu2.add(jrbVizszintes);
 
-        jrbFuggoleges.setSelected(true);
+        buttonGroup2.add(jrbFuggoleges);
         jrbFuggoleges.setText("függőleges");
         jMenu2.add(jrbFuggoleges);
 
@@ -266,14 +276,6 @@ public class gui extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jmiUjraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiUjraActionPerformed
-        pinField.setText("");
-        jrbKever.setSelected(false);
-        for (JButton jButton : gombokPin) {
-            jButton.setBackground(alapSzin);
-        }
-    }//GEN-LAST:event_jmiUjraActionPerformed
-
     private void jrbKeverItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jrbKeverItemStateChanged
         //kever
         if (jrbKever.isSelected()) {
@@ -287,19 +289,21 @@ public class gui extends javax.swing.JFrame {
 
     }//GEN-LAST:event_jrbKeverItemStateChanged
 
-    private void jmiKilepActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiKilepActionPerformed
-       int valasz = JOptionPane.showConfirmDialog(this, "Kilépés", "Biztos kilép?", JOptionPane.YES_NO_OPTION);
-       if(valasz == JOptionPane.YES_OPTION){
-           System.exit(valasz);
-       }
-    }//GEN-LAST:event_jmiKilepActionPerformed
-
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
         int valasz = JOptionPane.showConfirmDialog(this, "Kilépés", "Biztos kilép?", JOptionPane.YES_NO_OPTION);
-       if(valasz == JOptionPane.YES_OPTION){
-           System.exit(valasz);
-       }
+        if (valasz == JOptionPane.YES_OPTION) {
+            System.exit(valasz);
+        }
     }//GEN-LAST:event_formWindowClosing
+
+    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
+        jel = KERESZT;
+
+    }//GEN-LAST:event_jRadioButton3ActionPerformed
+
+    private void jRadioButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton4ActionPerformed
+        jel = KOR;
+    }//GEN-LAST:event_jRadioButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -337,7 +341,9 @@ public class gui extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel amobaPanel;
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JList<String> jList2;
     private javax.swing.JMenu jMenu2;
@@ -346,7 +352,6 @@ public class gui extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
     private javax.swing.JRadioButton jRadioButton3;
     private javax.swing.JRadioButton jRadioButton4;
     private javax.swing.JScrollPane jScrollPane2;
@@ -362,7 +367,7 @@ public class gui extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
 
     private void sajatInit() {
-        
+        menuAction();
         gombokPin = new JButton[10];
         for (int i = 0; i < gombokPin.length; i++) {
             gombokPin[i] = new JButton();
@@ -379,7 +384,7 @@ public class gui extends javax.swing.JFrame {
         }
         alapSzin = gombokPin[0].getBackground();
         alapGombPin();
-
+        amobaAlap();
     }
 
     private void kever() {
@@ -407,6 +412,73 @@ public class gui extends javax.swing.JFrame {
             gombokPin[i].setText("" + (i + 1));
         }
         gombokPin[gombokPin.length - 1].setText("" + 0);
+    }
+
+    private void amobaAlap() {
+        gombokAmoba = new ArrayList();
+
+        amobaGen(3);
+    }
+
+    private void amobaGen(int i) {
+        amobaPanel.setLayout(new GridLayout(i, i + 1));
+        jel = KERESZT;
+        for (int j = 0; j < i * i; j++) {
+            gombokAmoba.add(new JButton(" "));
+
+            amobaPanel.add((Component) gombokAmoba.get(j));
+            gombokAmoba.get(j).addActionListener(
+                    new java.awt.event.ActionListener() {
+                @Override
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    JButton gomb = (JButton) evt.getSource();
+                    gomb.setText("" + jel);
+                    if (jel == KERESZT) {
+                        jel = KOR;
+                    } else {
+                        jel = KERESZT;
+                    }
+
+                }
+            }
+            );
+        }
+
+    }
+
+    private void menuAction() {
+        ujra();
+        kilep();
+    }
+
+    private void ujra() {
+        jmiUjra.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pinField.setText("");
+                jrbKever.setSelected(false);
+                for (JButton jButton : gombokPin) {
+                    jButton.setBackground(alapSzin);
+                }
+
+            }
+        });
+
+    }
+
+    private void kilep() {
+        jmiKilep.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                int valasz;
+                valasz = JOptionPane.showConfirmDialog(frame, "Kilépés", "Biztos kilép?", JOptionPane.YES_NO_OPTION);
+                if (valasz == JOptionPane.YES_OPTION) {
+                    System.exit(valasz);
+                }
+
+            }
+        });
+
     }
 
 }
